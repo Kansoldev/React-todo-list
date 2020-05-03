c86ec752c40569cedeb5253a0ed2e1174ec72c87
Oyinkansola <yahayaoyinkansola@gmail.com>
2020-05-03 08:06:04 +0100

    Updates

---
+++ precache-manifest.ad569a6f5ca352e5383ea76152ac43e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67bd513851a0d97f9b41ef7ca1f90d9",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "f7e7e789b00a6055086f",
+    "url": "/React-todo-list/static/css/2.93d0fbd0.chunk.css"
+  },
+  {
+    "revision": "7431e597bed9e4b9ff9a",
+    "url": "/React-todo-list/static/css/main.ffe21cf2.chunk.css"
+  },
+  {
+    "revision": "f7e7e789b00a6055086f",
+    "url": "/React-todo-list/static/js/2.249db5ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-list/static/js/2.249db5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7431e597bed9e4b9ff9a",
+    "url": "/React-todo-list/static/js/main.67749df6.chunk.js"
+  },
+  {
+    "revision": "8b72dc4c06dfccfbd466",
+    "url": "/React-todo-list/static/js/runtime-main.8d2ec67b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-todo-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-todo-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-todo-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-todo-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-todo-list/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
